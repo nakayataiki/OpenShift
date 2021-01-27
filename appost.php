0024005a80903f7f685b7df18dca2d99f8e59ef8
nakayataiki <78003443+nakayataiki@users.noreply.github.com>
2021-01-27 12:44:28 +0900

    Update appost.php

--- appost.php
+++ appost.php
@@ -9,14 +9,14 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With");
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'https://s3.direct.jp-tok.cloud-object-storage.appdomain.cloud/4d135723cf2a4555ac2474948404495a:1ecc5e8b-fe42-4d19-9475-e928a2566db2:bucket:test-storage-cos-standard-4hq/');
+curl_setopt($ch, CURLOPT_URL, 'https://s3.direct.jp-tok.cloud-object-storage.appdomain.cloud/4d135723cf2a4555ac2474948404495a:1ecc5e8b-fe42-4d19-9475-e928a2566db2:bucket:test-storage-cos-standard-4hq/test');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, "test");
 
 $headers = array();
-//$headers[] = 'Authorization: bearer "token"';
+$headers[] = 'Authorization: bearer "ycZ0G85rB0eECfjlfIbM1nm90wqzGvOxQN3CWe07TmpN"';
 $headers[] = 'Content-Type: "application/json;charset=UTF-8"';
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
