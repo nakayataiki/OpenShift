2d6d6bd33a0eba540619fac64e21c1ff37fedbab
nakayataiki <78003443+nakayataiki@users.noreply.github.com>
2021-01-28 12:53:25 +0900

    Update imgreg.php

--- imgreg.php
+++ imgreg.php
@@ -3,28 +3,53 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With");
 
-$image = $_POST['image'];
+$data = array(
 
-$dirName = date('Ymd');
-$fileName = date('YmdHis');
-
+        'apikey'        => 'ytQi1gPzzvb-V6JfsYKRqx7o9Jgv1QHbGkTPcn77QS1C',
+        'response_type' => 'cloud_iam',
+        'grant_type'    => 'urn:ibm:params:oauth:grant-type:apikey'
+);
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://s3.direct.jp-tok.cloud-object-storage.appdomain.cloud/test-storage-cos-standard-qxx/' . $dirName . '/' . $fileName . '/' . 'txt');
+curl_setopt($ch, CURLOPT_URL, 'https://iam.cloud.ibm.com/identity/token');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-curl_setopt($ch, CURLOPT_POSTFIELDS, $image);
-
+curl_setopt($ch, CURLOPT_POST, 1);
 $headers = array();
-$headers[] = 'Authorization: bearer eyJraWQiOiIyMDIxMDEyMDE4MzUiLCJhbGciOiJSUzI1NiJ9.eyJpYW1faWQiOiJpYW0tU2VydmljZUlkLTY3ZTIxYmFlLWJlZmUtNGRhOC05NDQzLWVlNzZkMmNjNzZkOCIsImlkIjoiaWFtLVNlcnZpY2VJZC02N2UyMWJhZS1iZWZlLTRkYTgtOTQ0My1lZTc2ZDJjYzc2ZDgiLCJyZWFsbWlkIjoiaWFtIiwianRpIjoiYTkwYWFlNzctYWNlMy00NWQ1LWJmNWUtODM1MWJkOGJjZDk0IiwiaWRlbnRpZmllciI6IlNlcnZpY2VJZC02N2UyMWJhZS1iZWZlLTRkYTgtOTQ0My1lZTc2ZDJjYzc2ZDgiLCJuYW1lIjoidGVzdC1zdG9yYWdlLWNvcy1zdGFuZGFyZC1xeHgiLCJzdWIiOiJTZXJ2aWNlSWQtNjdlMjFiYWUtYmVmZS00ZGE4LTk0NDMtZWU3NmQyY2M3NmQ4Iiwic3ViX3R5cGUiOiJTZXJ2aWNlSWQiLCJhY2NvdW50Ijp7InZhbGlkIjp0cnVlLCJic3MiOiI0ZDEzNTcyM2NmMmE0NTU1YWMyNDc0OTQ4NDA0NDk1YSIsImZyb3plbiI6dHJ1ZX0sImlhdCI6MTYxMTc0NDM1NSwiZXhwIjoxNjExNzQ3OTU1LCJpc3MiOiJodHRwczovL2lhbS5jbG91ZC5pYm0uY29tL2lkZW50aXR5IiwiZ3JhbnRfdHlwZSI6InVybjppYm06cGFyYW1zOm9hdXRoOmdyYW50LXR5cGU6YXBpa2V5Iiwic2NvcGUiOiJpYm0gb3BlbmlkIiwiY2xpZW50X2lkIjoiZGVmYXVsdCIsImFjciI6MSwiYW1yIjpbInB3ZCJdfQ.Q_zdHlzEVYgBH8UkY9Xu9U0RaP7PndzamCAzhRXdBZvK3-ArmfK6QbO91TybVJwySGq8MBuW73zFXulokvok2NaTBMIGob6qKmO51d9d9Me1PBlZV2j-m81eZdBuVENW80pvYUk1zD6S0yNpB9z9KLEQE6PNq6t9VsQYPAVH5Vw50ZN5cE638fgAoL3OurgvSSSpU39x5jPQWgzxY1JpSgZQytds9hciVTLH4QPDJ7PSblwzHO7fCwi-no6XGybvsO660nJkQdJNCDcY9ucsokHVuljrSBodTO28twE_wSkQaKwTM7g9GxZcGexgqVAUHPvs8Xme20ecGBclRFQX9Q';
-// $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+$headers[] = 'Accept: application/json';
+$headers[] = 'Content-Type: application/x-www-form-urlencoded';
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+//curl_setopt($ch, CURLOPT_WRITEFUNCTION, callback);
+//curl_setopt($ch, CURLOPT_WRITEFUNCTION, $callback);
 
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
 curl_close($ch);
+$result_str = json_decode($result, true);
 
-$errorCode = "00";
+$image = $_POST['image'];
+if(strstr($image, 'data:image/jpeg;base64,')) {
+	
+	$headerlessimg = str_replace('data:image/jpeg;base64,', '', $image);
+	$image = base64_decode($headerlessimg);
+}
+$dirName = date('Ymd');
+$fileName = date('YmdHis');
 
-echo $errorCode;
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, 'https://s3.direct.jp-tok.cloud-object-storage.appdomain.cloud/test-storage-cos-standard-qxx/' . $dirName . '/' . $fileName . '.jpeg');
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'PUT');
+curl_setopt($ch2, CURLOPT_POSTFIELDS, $image);
+$headers = array();
+$headers[] = 'Authorization: bearer ' . $result_str["access_token"]; 
+$headers[] = 'Content-Type: application/x-www-form-urlencoded';
+curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers);
+
+$result2 = curl_exec($ch2);
+if (curl_errno($ch2)) {
+	echo 'Error:' . curl_error($ch2);
+	}
+curl_close($ch2);
+echo $result2 . PHP_EOL;
